c293a63afb72bd15b73addc1f6562c436c7aa129
ibrahim elsadt <ibrahimsadat999@gmail.com>
2023-07-14 12:04:08 +0300

    Initial Commit

---
+++ problem_2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+double function (double n){
+
+return sqrt(n);
+}
+int main()
+{
+   int  n=4;
+    printf("%f",function(n));
+    return 0;
+}
